ca5f6dab63922ba8330fac6424609c3ba1ee98fd
onurk03 <65676529+onurk03@users.noreply.github.com>
2022-03-07 19:54:19 -0600

    React Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="navBar">
+            <h1 className="title"> Drawing App</h1>
+        </div>
+    </div>
+  );
+}
+
+export default App;
